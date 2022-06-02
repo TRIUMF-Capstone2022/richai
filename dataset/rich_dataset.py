40dfc1b738f04ac4bed4bdb76001e96781020840
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-06-02 14:41:09 -0700

    refactor: update dataset for filters, demeaning

--- dataset/rich_dataset.py
+++ dataset/rich_dataset.py
@@ -1,15 +1,11 @@
-from cProfile import label
-import logging
 import mmap
-from operator import pos
-from webbrowser import get
 import h5py
 import torch
-from torch.utils.data import Dataset
 import numpy as np
+import pandas as pd
 import os
 from collections import defaultdict
-
+from torch.utils.data import Dataset
 from utils.helpers import compute_seq_id, get_config, get_logger
 
 logger = get_logger()
@@ -18,56 +14,61 @@ logger = get_logger()
 class RICHDataset(Dataset):
     """RICH pytorch dataset."""
 
-    def __init__(self, dset_path, val_split=None, test_split=None, seed=None):
+    def __init__(
+        self,
+        dset_path,
+        sample_file=None,
+        val_split=None,
+        test_split=None,
+        seed=None,
+    ):
 
         # set seed
         if seed:
             np.random.seed(seed)
 
         # read input dataset
-        with h5py.File(dset_path, 'r') as dfile:
+        with h5py.File(dset_path, "r") as dfile:
 
             for key in dfile.attrs:
-                logger.info('%s: %s', key, dfile.attrs[key])
+                logger.info("%s: %s", key, dfile.attrs[key])
 
             # Load the hit map into memory
-            self.hit_mapping = np.asarray(dfile['HitMapping'][:])
-            logger.info('hit map size: %i bytes', self.hit_mapping.nbytes)
-            self.N = (
-                len(self.hit_mapping) - 1
-            )  # !!! The last cell is the sentinel !!!
+            self.hit_mapping = np.asarray(dfile["HitMapping"][:])
+            logger.info("hit map size: %i bytes", self.hit_mapping.nbytes)
+            self.N = len(self.hit_mapping) - 1
 
             # Get the info we need to memory map the hits
-            hit_ds = dfile['Hits']
+            hit_ds = dfile["Hits"]
             hit_offset = hit_ds.id.get_offset()
             hit_dtype = hit_ds.dtype
             hit_shape = hit_ds.shape
             hit_length = np.prod(hit_shape)
 
             # Get the info we need to memory map the events
-            event_ds = dfile['Events']
+            event_ds = dfile["Events"]
             event_offset = event_ds.id.get_offset()
             event_dtype = event_ds.dtype
             event_shape = event_ds.shape
             event_length = np.prod(event_shape)
 
             # Add labels
-            mu_off = dfile.attrs['muon_offset']
-            pi_off = dfile.attrs['pion_offset']
-            pos_off = dfile.attrs['positron_offset']
-            entries = dfile.attrs['entries']
+            mu_off = dfile.attrs["muon_offset"]
+            pi_off = dfile.attrs["pion_offset"]
+            pos_off = dfile.attrs["positron_offset"]
+            entries = dfile.attrs["entries"]
 
             if [mu_off, pi_off, pos_off] != sorted([mu_off, pi_off, pos_off]):
-                raise Exception('Offsets are not correct')
+                raise Exception("Offsets are not correct")
 
             self.offsets = {
-                'entries': entries,
-                'muon': mu_off,
-                'pion': pi_off,
-                'positron': pos_off,
+                "entries": entries,
+                "muon": mu_off,
+                "pion": pi_off,
+                "positron": pos_off,
             }
 
-            logger.info(f'Offsets: {self.offsets}')
+            logger.info(f"Offsets: {self.offsets}")
 
             # muon: 0, pion: 1, positron: 2
             self.labels = np.zeros(entries, dtype=np.int32)
@@ -75,14 +76,46 @@ class RICHDataset(Dataset):
             self.labels[pi_off:pos_off] = 1
             self.labels[pos_off:] = 2
 
-            logger.info(f'Entries: {entries}')
-            logger.info(f'Muons start at index: {mu_off}')
-            logger.info(f'Pions start at index: {pi_off}')
-            logger.info(f'Positron start at index: {pos_off}')
+            logger.info(f"Entries: {entries}")
+            logger.info(f"Muons start at index: {mu_off}")
+            logger.info(f"Pions start at index: {pi_off}")
+            logger.info(f"Positron start at index: {pos_off}")
+
+            # Get indices
+            if sample_file:
+                logger.info(f"Filtering indices for sample: {sample_file}")
+
+                df = pd.read_hdf(sample_file)
+                logger.info(f"Total samples: {df.shape[0]}")
+
+                # filter outliers
+                df = df.query("ring_centre_pos_x < 2500 and ring_centre_pos_y < 2500")
+                df = df.query("ring_centre_pos_x > -2500 and ring_centre_pos_y > -2500")
+
+                logger.info(f"Total samples with no outliers: {df.shape[0]}")
+
+                self.mean_centre_x = df["ring_centre_pos_x"].mean()
+                self.mean_centre_y = df["ring_centre_pos_y"].mean()
+
+                logger.info(
+                    f"Mean centre locations: ({self.mean_centre_x:.2f},{self.mean_centre_y:.2f})"
+                )
+
+                indices = df["original_index"].to_numpy()
+
+                # remove df from memory
+                del df
+            else:
+                indices = np.arange(self.N - 2)
 
             # shuffle indices
-            indices = np.arange(self.N - 2)
             np.random.shuffle(indices)
+
+            logger.info(f"Total indices: {len(indices)}")
+
+            self.train_indices = []
+            self.test_indices = []
+            self.val_indices = []
 
             # train, validation, test
             if test_split:
@@ -96,29 +129,36 @@ class RICHDataset(Dataset):
                 self.train_indices = indices[:-n_val]
                 self.val_indices = indices[-n_val:]
 
+        logger.info(f"Total Train indices: {len(self.train_indices)}")
+        logger.info(f"Total Validation indices: {len(self.val_indices)}")
+        logger.info(f"Total Test indices: {len(self.test_indices)}")
+
+        # for reproducibility (to check that test set is the same)
+        logger.info(f"First 10 Train indices: {self.train_indices[:10]}")
+        logger.info(f"First 10 Validation indices: {self.val_indices[:10]}")
+        logger.info(f"First 10 Test indices: {self.test_indices[:10]}")
+
         # We don't attempt to catch exception here, crash if we cannot open the file.
-        with open(dset_path, 'rb') as fh:
+        with open(dset_path, "rb") as fh:
             fileno = fh.fileno()
             mapping = mmap.mmap(fileno, 0, access=mmap.ACCESS_READ)
             self.hit_array = np.frombuffer(
                 mapping, dtype=hit_dtype, count=hit_length, offset=hit_offset
             ).reshape(hit_shape)
-            logger.info('hit array mmap size: %i bytes', self.hit_array.nbytes)
+            logger.info("hit array mmap size: %i bytes", self.hit_array.nbytes)
             self.event_array = np.frombuffer(
                 mapping,
                 dtype=event_dtype,
                 count=event_length,
                 offset=event_offset,
             ).reshape(event_shape)
-            logger.info(
-                'event array mmap size: %i bytes', self.event_array.nbytes
-            )
+            logger.info("event array mmap size: %i bytes", self.event_array.nbytes)
 
     def get_position_data(self):
-        return np.load('rich_pmt_positions.npy')
+        return np.load("rich_pmt_positions.npy")
 
     def get_event_pos(self, idx):
-
+        """Get the hits data for a single event."""
         # get hits
         idx_from = self.hit_mapping[idx]
         idx_to = self.hit_mapping[idx + 1]
@@ -133,18 +173,32 @@ class RICHDataset(Dataset):
 
         # add difference between chod_time and hit_time as 3rd dim
         event_pos[:, 2] = (
-            self.event_array[idx]['chod_time']
-            - self.hit_array[idx_from:idx_to]['hit_time']
+            self.event_array[idx]["chod_time"]
+            - self.hit_array[idx_from:idx_to]["hit_time"]
         )
 
         if event_pos.shape[0] > position_map.shape[0]:
-            logger.warning('Unusual event pos')
+            logger.warning("Unusual event pos")
             return position_map
 
+        # filter events for small time deltas
+        delta = get_config("dataset.delta")
+        event_pos = self.filter_events_delta(event_pos, delta)
+
+        # demean x and y locations
+        event_pos[:, :1] = event_pos[:, :1] - self.mean_centre_x
+        event_pos[:, 1:2] = event_pos[:, 1:2] - self.mean_centre_y
+
+        # pad with zeros
         data = np.zeros_like(position_map)
         data[: event_pos.shape[0], : event_pos.shape[1]] = event_pos
 
         return data
+
+    def filter_events_delta(self, event, delta):
+        """Filter hits array based on a delta = hit time - chod time"""
+        mask = (np.abs(event[:, 2:]) < delta).flatten()
+        return event[mask]
 
     def __len__(self):
         return self.N
@@ -154,32 +208,32 @@ class RICHDataset(Dataset):
         idx_to = self.hit_mapping[idx + 1]
 
         self.data = {
-            'event_pos': self.get_event_pos(idx),
-            'label': self.labels[idx],
-            'hit_time': self.hit_array[idx_from:idx_to]['hit_time'],
-            'chod_time': self.event_array[idx]['chod_time'],
-            'track_momentum': self.event_array[idx]['track_momentum'],
-            'ring_radius': self.event_array[idx]['ring_radius'],
+            "event_pos": self.get_event_pos(idx),
+            "label": self.labels[idx],
+            "hit_time": self.hit_array[idx_from:idx_to]["hit_time"],
+            "chod_time": self.event_array[idx]["chod_time"],
+            "track_momentum": self.event_array[idx]["track_momentum"],
+            "ring_radius": self.event_array[idx]["ring_radius"],
         }
 
         return (
-            torch.tensor(self.data['event_pos']),
-            torch.tensor(self.data['label']),
-            torch.tensor(self.data['track_momentum']),
+            torch.tensor(self.data["event_pos"]),
+            torch.tensor(self.data["label"]),
+            torch.tensor(self.data["track_momentum"]),
         )
 
 
-def combine_datset(key, **kwargs):
+def combine_dataset(key, prefix="patched", **kwargs):
     """Combine all the datasets on specified path
     key: train or test
     """
     # get it from config
     dset_dirs = [
-        os.path.join(get_config('dataset.base_dir'), i)
-        for i in get_config(f'dataset.{key}')
+        os.path.join(get_config("dataset.base_dir"), i)
+        for i in get_config(f"dataset.{key}")
     ]
 
-    logger.info(f'Train directories: {dset_dirs}')
+    logger.info(f"Train directories: {dset_dirs}")
 
     # file list
     dset_dict = defaultdict()
@@ -187,25 +241,28 @@ def combine_datset(key, **kwargs):
     for dset_dir in dset_dirs:
         # check if the directory exists
         if not os.path.exists(dset_dir):
-            raise Exception(f'Directory {dset_dir} does not exist')
+            raise Exception(f"Directory {dset_dir} does not exist")
 
         # get list of files
         for dset_file in os.listdir(dset_dir):
+            # check if prefix in kwargs and file name contains prefix
+            if prefix and prefix not in dset_file:
+                logger.info(f"Skipping {dset_file}")
+                continue
             file_ = os.path.join(dset_dir, dset_file)
-            dset_dict[file_] = RICHDataset(file_, **kwargs)
+            dset_dict[file_] = RICHDataset(
+                file_,
+                sample_file="/fast_scratch_1/capstone_2022/datasetC_combined.h5",
+                **kwargs,
+            )
 
-    logger.info(f'Training files: {dset_dict}')
+    logger.info(f"Training files: {dset_dict}")
 
     return dset_dict
 
 
-if __name__ == '__main__':
-    combine_datset('test')
-    # path = os.path.join(
-    #     get_config("dataset.base_dir"),
-    #     "A",
-    #     "Run008548.EOSlist.CTRL.p.v2.0.4-01_f.v2.0.4-01.h5",
-    # )
-    # dset = RICHDataset(dset_path=path, val_split=0.1, test_split=0.1)
-    # print(dset[0])
-    # print(dset.data)
+if __name__ == "__main__":
+    logger.info(f"Test for training dataset")
+    combine_dataset("train", val_split=0.2, test_split=0.1)
+    logger.info(f"Test for test dataset")
+    combine_dataset("test")
