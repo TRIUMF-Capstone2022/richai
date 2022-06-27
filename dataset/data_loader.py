ff501379a37a3acdb318b7cd8a833af22520d9c3
Rakesh Pandey <rakeshpandey820@gmail.com>
2022-06-26 19:20:40 -0700

    move pos data to dataset

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -22,31 +22,31 @@ def data_loader(dset):
     """
     train_loader = DataLoader(
         dset,
-        batch_size=get_config("data_loader.batch_size"),
+        batch_size=get_config('data_loader.batch_size'),
         shuffle=False,
         sampler=SubsetRandomSampler(dset.train_indices),
-        num_workers=get_config("data_loader.num_workers"),
-        drop_last=get_config("data_loader.drop_last"),
+        num_workers=get_config('data_loader.num_workers'),
+        drop_last=get_config('data_loader.drop_last'),
     )
 
     val_loader = DataLoader(
         dset,
-        batch_size=get_config("data_loader.batch_size"),
+        batch_size=get_config('data_loader.batch_size'),
         shuffle=False,
         sampler=SubsetRandomSampler(dset.val_indices),
-        num_workers=get_config("data_loader.num_workers"),
-        drop_last=get_config("data_loader.drop_last"),
+        num_workers=get_config('data_loader.num_workers'),
+        drop_last=get_config('data_loader.drop_last'),
     )
 
     test_loader = None
-    if hasattr(dset, "test_indices"):
+    if hasattr(dset, 'test_indices'):
         test_loader = DataLoader(
             dset,
-            batch_size=get_config("data_loader.batch_size"),
+            batch_size=get_config('data_loader.batch_size'),
             shuffle=False,
             sampler=SubsetRandomSampler(dset.test_indices),
-            num_workers=get_config("data_loader.num_workers"),
-            drop_last=get_config("data_loader.drop_last"),
+            num_workers=get_config('data_loader.num_workers'),
+            drop_last=get_config('data_loader.drop_last'),
         )
 
     return (train_loader, val_loader, test_loader)
